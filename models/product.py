662e7ba1deb1be15d1333bbd86ce339bcfd77cf6
tawheed78 <tawheed.chilwan55@gmail.com>
2023-08-06 13:03:39 +0530

    added product models

---
+++ models/product.py
@@ -0,0 +1,32 @@
+from typing import List
+from pydantic import BaseModel
+
+class Size(BaseModel):
+    length: int
+    breadth: int
+    height: int
+
+class Review(BaseModel):
+    title: str
+    description: str
+    rating: float
+
+class Price(BaseModel):
+    mrp: float
+    discounted_price: float
+
+class Product(BaseModel):
+    title: str
+    brand: str
+    category: str
+    model: str
+    size: List[Size]
+    seller: str
+    price: Price
+    review: List[Review] | None=None
+    mfg_date: str
+    is_returnable: bool
+    is_available: bool
+    avail_qunatity: int
+    
+
